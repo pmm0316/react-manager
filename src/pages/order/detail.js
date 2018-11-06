e33810cd8a8d8c9c0343dbae13c42b28396bb37a
pmm0316 <847679250@qq.com>
2018-11-06 22:47:10 +0800

    嵌入百度地图

--- src/pages/order/detail.js
+++ src/pages/order/detail.js
@@ -33,15 +33,48 @@ export default class Order extends React.Component {
         this.setState({
           orderInfo: res.result
         })
+        this.renderMap(res.result)
       }
     })
+  }
+  renderMap = (result) => {
+    this.map = new window.BMap.Map('orderDetailMap')
+    this.map.centerAndZoom('北京', 11)
+    // 添加地图控件
+    this.addMapControl()
+    this.drawBikeRoute(result.positionList)
+
+  }
+  addMapControl = () => {
+    let map = this.map
+    map.addControl(new window.BMap.ScaleControl({anchor: window.BMAP_ANCHOR_TOP_RIGHT}))
+    map.addControl(new window.BMap.NavigationControl({anchor: window.BMAP_ANCHOR_TOP_RIGHT}))
+  }
+  drawBikeRoute = (positionList) => {
+    let map = this.map
+    let startPoint = ''
+    let endPoint = ''
+    if (positionList.length) {
+      let arr = positionList[0]
+      startPoint = new window.BMap.Point(arr.lon, arr.lat)
+      let startIcon = new window.BMap.Icon(
+        '/assets/start_point.png',
+        new window.BMap.Size(36, 42),
+        {
+          imageSize: new window.BMap.Size(36, 42),
+          anchor: new window.BMap.Size(36, 42)
+        }
+      )
+      let startMarker = new window.BMap.Marker(startPoint, {icon: startIcon})
+      this.map.addOverlay(startMarker)
+    }
   }
   render() {
     const info = this.state.orderInfo || {}
     return(
       <div>
         <Card>
-          <div id="orderDetailMap"></div>
+          <div id="orderDetailMap" className="order-map"></div>
           <div className="detail-items">
             <div className="item-title">基础信息</div>
             <ul className="detail-form">
